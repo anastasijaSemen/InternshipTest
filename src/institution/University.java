3334de3131da6ae946417a99671985c80b320bcd
Anastasija Semen <anastasija.semen@gmail.com>
2018-10-06 01:04:02 +0300

    First commit

---
+++ src/institution/University.java
@@ -0,0 +1,18 @@
+package institution;
+
+import person.Student;
+
+public class University {
+
+    public University(String name) {
+        //TODO: Implementation is needed
+    }
+
+    public void setStudent(Student student) {
+        //TODO: Implementation is needed
+    }
+
+    public void addStudent(Student student) {
+        //TODO: Implementation is needed
+    }
+}
