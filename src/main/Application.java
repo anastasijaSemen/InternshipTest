673e3d61346f542eedbbce3ebe11e0fb33b6898f
Anastasija Semen <anastasija.semen@gmail.com>
2018-10-11 01:44:41 +0300

    Set knowledges to student

--- src/main/Application.java
+++ src/main/Application.java
@@ -3,14 +3,28 @@ package main;
 import institution.University;
 import institution.interlink.Internship;
 import person.Student;
+import person.consciousness.Knowledge;
 
 public class Application {
     public static void main(String[] args) {
         University university = new University("CH.U.I.");
 
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
+        Student student = new Student("Julia Veselkina");
+        student.setKnowledge(new Knowledge(193));
+        university.addStudent(student);
+
+        student = new Student("Maria Perechrest");
+        student.setKnowledge(new Knowledge(134));
+        university.addStudent(student);
+
+        student = new Student("Andrew Maslenko");
+        student.setKnowledge(new Knowledge(178));
+        university.addStudent(student);
+
+        student = new Student("Andrew Kostenko");
+        student.setKnowledge(new Knowledge(145));
+        university.addStudent(student);
+
 
         Internship internship = new Internship("Interlink");
         System.out.println("List of internship's students:");
